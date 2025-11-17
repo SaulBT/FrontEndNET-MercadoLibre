0d9a29e8307db5488e2ecf01f051e3d2c445a827
SaulBT <barragansaul26n@gmail.com>
2025-11-17 16:05:30 -0600

    feat(Models): Se agregaron todos los modelos

---
+++ Models/Archivo.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace frontendnet.Models;
+
+public class Archivo
+{
+    [Display(Name = "Id")]
+    public int? ArchivoId { get; set; }
+
+    [Display(Name = "MIME")]
+    public string? Mime { get; set; }
+
+    [Display(Name = "Tamprecio")]
+    public int? Size { get; set; }
+
+    [Display(Name = "Repositorio")]
+    public bool InDb { get; set; } = true;
+}
